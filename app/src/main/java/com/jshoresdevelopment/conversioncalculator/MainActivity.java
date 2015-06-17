b9d665661754566ca5cad7a9717dcf02e9a4060a
jsho32 <justin.shores32@gmail.com>
2015-06-16 20:18:10 -0700

    Implement frag switcher and conversion set type set up.

--- app/src/main/java/com/jshoresdevelopment/conversioncalculator/MainActivity.java
+++ app/src/main/java/com/jshoresdevelopment/conversioncalculator/MainActivity.java
@@ -3,7 +3,6 @@ package com.jshoresdevelopment.conversioncalculator;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,12 +14,9 @@ import java.util.List;
 
 
 public class MainActivity extends FragmentActivity {
-    private TextView distance;
-    private TextView volume;
-    private TextView weight;
     private List<TextView> navButtons;
     private Fragment currentFragment;
-    private MainFragment mainFragment;
+    private ConversionFragment conversionFragment;
 
 
     @Override
@@ -28,35 +24,24 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainFragment = new MainFragment();
+        MainFragment mainFragment = new MainFragment();
+        conversionFragment = new ConversionFragment();
+        initializeNavBar();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.content, mainFragment)
                 .commitAllowingStateLoss();
         currentFragment = mainFragment;
 
-        initializeNavBar();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onResume() {
+        super.onResume();
+        MainFragment mainFragment = new MainFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, mainFragment)
+                .commitAllowingStateLoss();
+        currentFragment = mainFragment;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /** Resets nav bar buttons to original colors. */
@@ -71,35 +56,39 @@ public class MainActivity extends FragmentActivity {
     private void initializeNavBar() {
         navButtons = new ArrayList<>();
 
-        distance = (TextView) findViewById(R.id.distance_button);
-        setNavButtonsOnClickListener(distance);
+        TextView distance = (TextView) findViewById(R.id.distance_button);
+        setNavButtonsOnClickListener(distance, "Distance");
         navButtons.add(distance);
 
-        volume = (TextView) findViewById(R.id.volume_button);
-        setNavButtonsOnClickListener(volume);
+        TextView volume = (TextView) findViewById(R.id.volume_button);
+        setNavButtonsOnClickListener(volume, "Volume");
         navButtons.add(volume);
 
-        weight = (TextView) findViewById(R.id.weight_button);
-        setNavButtonsOnClickListener(weight);
+        TextView weight = (TextView) findViewById(R.id.weight_button);
+        setNavButtonsOnClickListener(weight, "Weight");
         navButtons.add(weight);
     }
 
     /** Standardized nav bar on click listener. */
-    private void setNavButtonsOnClickListener(final TextView view) {
+    private void setNavButtonsOnClickListener(final TextView view, final String headText) {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                conversionFragment.setConversionType(headText);
                 resetNavButtons();
                 view.setBackgroundColor(Color.parseColor("#ff007223"));
                 view.setTextColor(Color.WHITE);
+
+                if (currentFragment != conversionFragment) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, conversionFragment).commitAllowingStateLoss();
+                    currentFragment = conversionFragment;
+                } else {
+                    if (conversionFragment.getConversionHead() != null) {
+                        conversionFragment.setLayoutResources();
+                    }
+                }
             }
         });
     }
 
-    /** Switch UI to the given fragment. */
-    void switchToFragment(Fragment newFrag) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.content, newFrag)
-                .commitAllowingStateLoss();
-        currentFragment = newFrag;
-    }
 }
