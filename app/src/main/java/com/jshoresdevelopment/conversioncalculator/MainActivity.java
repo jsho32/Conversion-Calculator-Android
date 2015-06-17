7a95e2af02cc1b7aac4fe763645d56ede4a9f485
jsho32 <justin.shores32@gmail.com>
2015-06-17 01:16:37 -0700

    Implement adview and developer buttons.

--- app/src/main/java/com/jshoresdevelopment/conversioncalculator/MainActivity.java
+++ app/src/main/java/com/jshoresdevelopment/conversioncalculator/MainActivity.java
@@ -1,13 +1,17 @@
 package com.jshoresdevelopment.conversioncalculator;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +22,6 @@ public class MainActivity extends FragmentActivity {
     private Fragment currentFragment;
     private ConversionFragment conversionFragment;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +30,55 @@ public class MainActivity extends FragmentActivity {
         MainFragment mainFragment = new MainFragment();
         conversionFragment = new ConversionFragment();
         initializeNavBar();
+        initializeButtons();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.content, mainFragment)
                 .commitAllowingStateLoss();
         currentFragment = mainFragment;
+
+        // TODO change adUnitId in layout resource when app is uplaoded to play
+        AdView adView = (AdView) this.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        MainFragment mainFragment = new MainFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.content, mainFragment)
-                .commitAllowingStateLoss();
-        currentFragment = mainFragment;
+        if (conversionFragment.getConversionHead() == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, mainFragment)
+                    .commitAllowingStateLoss();
+            currentFragment = mainFragment;
+        }
+    }
 
+    /** Initializes all buttons. */
+    private void initializeButtons() {
+        Button more = (Button) findViewById(R.id.more);
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/developer?id=J.+Shores+Development");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        Button share = (Button) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
+                String sAux = "Download it here: \n\n";
+                sAux = sAux + "https://play.google.com/store/apps/details?id=com.jshoresdevelopment.conversioncalculator.app \n\n";
+                intent.putExtra(Intent.EXTRA_TEXT, sAux);
+                startActivity(Intent.createChooser(intent, "Choose Sharing Medium"));
+            }
+        });
     }
 
     /** Resets nav bar buttons to original colors. */
